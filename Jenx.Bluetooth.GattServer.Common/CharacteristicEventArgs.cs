6d5cad5cc32b5691a9428cbe2b482674be8563ea
Bora Eskicioğlu <76914128+Besk61@users.noreply.github.com>
2021-12-20 00:30:18 +0300

    Add files via upload

---
+++ Jenx.Bluetooth.GattServer.Common/CharacteristicEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Jenx.Bluetooth.GattServer.Common
+{
+    public class CharacteristicEventArgs : EventArgs
+    {
+        private string v1;
+        private string v2;
+
+        public CharacteristicEventArgs(Guid characteristicId, object value)
+        {
+            Characteristic = characteristicId;
+            Value = value;
+            //Value = "bora";
+        }
+
+        public CharacteristicEventArgs(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public Guid Characteristic { get; set; }
+
+        public object Value { get; set; }
+    }
+}
